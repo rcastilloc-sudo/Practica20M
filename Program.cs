eb7d2bfac761b28b3a39700c3e7ce172d80fe1c3
rcastilloc-sudo <rjcastilloc@uamv.edu.ni>
2026-05-20 09:53:23 -0600

    docs: indicaciones

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿//crear una aplicacion que guarde los registros de notas de 25 estudiantes, debe presentar un menu con las siguientes opciones: 1. Agregar estudiante, 2. Mostrar estudiantes, 3. mostrar los primeros 3 lugares, 4.mostrar las notas en orden ascendente, 5. salir
+﻿//crear una aplicacion que guarde los registros de notas de 25 estudiantes, debe presentar un menu con las siguientes opciones: 1. Agregar estudiante, 2. Mostrar estudiantes, 3. mostrar los primeros 3 lugares, 4.mostrar las notas en orden descendente, 5. salir
 
+int[] notas = new int[25];
+
+/*Agregar*/
+
+/*Mostrar*/
+
+/*3 primeros lugares*/
+
+/*Mostrar notas en orden descendente*/
